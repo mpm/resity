9b67af32ed736eed226919438404e98bdc5e38f7
Malte Münchert <malte.muenchert@gmx.net>
2015-04-08 23:59:28 +0200

    Extend demo format

--- lib/resity/format/text.rb
+++ lib/resity/format/text.rb
@@ -6,6 +6,22 @@ module Resity
     # In practice you might want to use a more powerful engine like git
     # for this, but it will suffice to get the idea.
     class Text < Base
+
+      require 'bindata'
+
+      class TextHeader < ::BinData::Record
+        endian :little
+        uint32 :currency_conversion
+        uint16 :bids_count
+        uint16 :asks_count
+      end
+
+      class TextRecord < ::BinData::Record
+        endian :little
+        # FIXME: datatype
+        double_le :amount
+        double_le :price
+      end
       attr_reader :last_data, :current_timestamp
 
       def initialize
